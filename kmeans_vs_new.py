80940152e6786ffe8990c357951577d9cccf976a
amatur <amatur003@gmail.com>
2016-12-13 17:17:25 +0600

    wrapping up, clustering done, fixed empty cluster error

--- kmeans_vs_new.py
+++ kmeans_vs_new.py
@@ -3,20 +3,19 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm 
 import matplotlib
 
+np.random.seed(10)
+
 def takeInput(filename="bisecting.txt", delimiter="\t"):
     point_list = []
     with open(filename, "r") as f:
         line = f.readline()
-        print line
         while((line.rstrip()) != ""):
             x, y = line.strip().split(delimiter)            
             x = float(x)
             y = float(y)
             line = f.readline()
-            point_list.append((x, y))
-        
+            point_list.append((x, y))        
     return point_list
-
 
 class Cluster:
     def __init__(self, point_list, centroid_tuple=(0,0)):
@@ -30,6 +29,7 @@ class Cluster:
 
     def updateCentroid(self):
         if (len(self.point_list)!=0):
+            '''
             sumx = 0
             sumy = 0
             for tup in self.point_list:
@@ -38,23 +38,94 @@ class Cluster:
             sumx /= len(self.point_list)
             sumy /= len(self.point_list)
             self.centroid = (sumx, sumy)
+            '''
+            self.centroid = np.average(self.point_list, axis=0)
         return self.centroid
 
     def SSE(self):
         sse = 0
         for tup in self.point_list:
-            x = tup[0]
-            y = tup[1]
             sse += (self.centroid[0] - tup[0])**2 + (self.centroid[1] - tup[1])**2 
         return sse
     
     def distance(self, point):
         return ((self.centroid[0] - point[0])**2 + (self.centroid[1] - point[1])**2)
+    
 
-import numpy as np
+def kmeans(point_list, k, centroids=[], SIMULATION = False):
+    nplist = np.asarray(point_list , dtype='float') # [[1,2],[2,4],[3,6]]
+    xlist = nplist[0: , 0]  # [1, 2, 3]
+    ylist = nplist[0: , 1]  # [2, 4, 6]
+    label = np.zeros(len(xlist), dtype='int32') # [0, 0, 0]
 
+    #selecting k points as initial centroid
+    maxx, maxy = np.max(nplist, axis = 0)
+    minx, miny = np.min(nplist, axis = 0)
 
-def bisectingKmeans(point_list, k):
+    if(len(centroids)!=k):   
+        centroids = []
+        for i in xrange(k):
+            ranx = np.random.uniform(minx, maxx)
+            rany = np.random.uniform(miny, maxy)
+            centroids.append((ranx, rany))
+        centroids =  np.asarray(centroids , dtype='float')
+
+    ####plt.scatter(xlist, ylist)
+    ####plt.plot(centroids[0: , 0], centroids[0: , 1], '+', mew=5, ms=20, color='r' )
+    ####plt.show()
+    
+    # initial k clusters
+    clusters = []
+    cluster_colors =(cm.rainbow(np.linspace(0,1,k)))
+    for i in xrange(k):
+        c = Cluster([], centroids[i])
+        c.color = cluster_colors[i]
+        clusters.append(c) 
+
+    # repeat
+    # form k clusters by assigning all points to the closest centroid
+    # recompute the centroid
+    # until the centroids dnt change
+    breakflag = False
+    figiter = 0
+    numpoints = len(point_list)
+    while(True):
+        if breakflag:
+            break         
+        for ip in xrange(numpoints):
+            point = point_list[ip]
+            dists = [0.0]*k        
+            for i in xrange(k):
+                dists[i] = (clusters[i].distance(point))
+            min_dist_k = np.argmin(dists)
+            label[ip] = min_dist_k
+            clusters[min_dist_k].point_list.append(point)
+
+        new_centroids = [(0,0)]*k    
+        for i in xrange(k):        
+            new_centroids[i] = (clusters[i].updateCentroid())
+
+        new_centroids = np.asarray(new_centroids, dtype=float)
+        if (np.allclose(centroids, new_centroids, .001, .001)):
+            breakflag = True    
+        centroids = new_centroids
+        figiter = figiter + 1
+        #print figiter
+
+    if(SIMULATION):
+        t=(cm.rainbow(np.linspace(0,1,len(xlist))))
+        for i in xrange(len(xlist)):
+            t[i] = clusters[label[i]].color
+        plt.scatter(xlist,ylist, c=t)
+        for i,cc in zip(range(k), cluster_colors):
+            plt.plot(centroids[i][0],centroids[i][1], '+', mew=10, ms=25,c='black')
+            plt.plot(centroids[i][0],centroids[i][1], '+', mew=5, ms=20,c=cc)
+        plt.title("K-Means " + str(figiter))
+        plt.show()
+        
+    return clusters
+
+def bisectingKmeans(point_list, k, SIMULATION = False):
     output_clusters = []
     c = Cluster(point_list)
     list_clusters = [c]
@@ -68,7 +139,7 @@ def bisectingKmeans(point_list, k):
 
         pick = list_clusters[picked_cluster_i]
         min_sum_sse = float('inf')  
-        for i in range(10):
+        for i in xrange(10):
             clusters = kmeans(pick.point_list, 2)
             sum_sse = clusters[0].SSE() + clusters[1].SSE()
             if sum_sse < min_sum_sse:
@@ -79,144 +150,37 @@ def bisectingKmeans(point_list, k):
         list_clusters.append(best_clusters[1])
 
         cluster_colors = cm.rainbow(np.linspace(0,1,len(list_clusters)))        
-        for i in range(len(list_clusters)):
+        for i in xrange(len(list_clusters)):
             list_clusters[i].color = cluster_colors[i]
 
-        for i in range(len(list_clusters)):
-            nppoints = np.asarray(list_clusters[i].point_list, dtype = float)
-            nplist = np.asarray(point_list , dtype='float')
-            plt.scatter(nppoints[0: , 0], nppoints[0: , 1], c=list_clusters[i].color)            
-            plt.plot(list_clusters[i].centroid[0],list_clusters[i].centroid[1], '+', mew=10, ms=25,c='black')
-            plt.plot(list_clusters[i].centroid[0],list_clusters[i].centroid[1], '+', mew=5, ms=20,c=list_clusters[i].color)
-        plt.show()
-             
+        if (SIMULATION):
+            for i in xrange(len(list_clusters)):
+                nppoints = np.asarray(list_clusters[i].point_list, dtype = float)
+                plt.scatter(nppoints[0: , 0], nppoints[0: , 1], c=list_clusters[i].color)            
+                plt.plot(list_clusters[i].centroid[0],list_clusters[i].centroid[1], '+', mew=10, ms=25,c='black')
+                plt.plot(list_clusters[i].centroid[0],list_clusters[i].centroid[1], '+', mew=5, ms=20,c=list_clusters[i].color)
+            plt.title("Bisecting K-Means")
+            plt.show()
 
-def kmeans(point_list, k):
-    nplist = np.asarray(point_list , dtype='float')
+    return list_clusters            
 
-    xlist = nplist[0: , 0]  # [1, 2, 3]
-    ylist = nplist[0: , 1]  # [2, 4, 6]
-    label = np.zeros(len(xlist), dtype='int32') # [0, 0, 0]
-
-    #selecting k points as initial centroid
-    maxx, maxy = np.max(nplist, axis = 0)
-    minx, miny = np.min(nplist, axis = 0)
-    centroids = []    
-    for i in range(k):
-        ranx = np.random.uniform(minx, maxx)
-        rany = np.random.uniform(miny, maxy)
-        centroids.append((ranx, rany))
-    centroids =  np.asarray(centroids , dtype='float')
-    ####plt.scatter(xlist, ylist)
-    ####plt.plot(centroids[0: , 0], centroids[0: , 1], '+', mew=5, ms=20, color='r' )
-    ####plt.show()
-    #t = np.arange(len(xlist))
-    
-    cluster_colors =(cm.rainbow(np.linspace(0,1,k)))
-    #plt.plot(centroids[0: , 0], centroids[0: , 1], '+', mew=5, ms=20, c=cluster_colors )
-    #plt.show()
-
-    # initial k clusters
-    clusters = []
-
-    for i in range(k):
-        c = Cluster([], centroids[i])
-        c.color = cluster_colors[i]
-        clusters.append(c) 
-
-    # repeat
-    # form k clusters by assigning all points to the closest centroid
-    # recompute the centroid
-    # until the centroids dnt change
-    breakflag = False
-    for iter in range(20):   
-        if breakflag:
-            break         
-        for ip in range(len(point_list)):
-            point = point_list[ip]
-            dists = []        
-            for i in range(k):
-                dists.append(clusters[i].distance(point))
-            min_dist_k = np.argmin(dists)
-            label[ip] = min_dist_k
-            clusters[min_dist_k].point_list.append(point)
-
-        new_centroids = []
-        for i in range(k):        
-            new_centroids.append(clusters[i].updateCentroid())
-        #centroids = np.asarray(centroids, dtype=float)
-        new_centroids = np.asarray(new_centroids, dtype=float)
-        if (np.allclose(centroids, new_centroids)):
-            breakflag = True    
-        centroids = new_centroids
-
-        
-        #for i in range(k):
-        #    col = [1,2,2,2]
-        #    col[0] = Blues(clusters[i].color)[0] * 255
-        #    col[1] = Blues(clusters[i].color)[1] * 255
-        #    col[2] = Blues(clusters[i].color)[2] * 255
-        #    col[3] = Blues(clusters[i].color)[3] * 255
-        #    col = Blues(clusters[i].color)
-        #    t2.append(col)
-       # plt.plot(3, 5, mew = 5, ms = 20, color = t[0])
-        #plt.plot(centroids[0: , 0], centroids[0: , 1], '+', mew=5, ms=20, color='r' )
-        
-        
-    t=(cm.rainbow(np.linspace(0,1,len(xlist))))
-    for i in range(len(xlist)):
-        t[i] = clusters[label[i]].color
-    #for i,tt in zip(range(k), t):
-    ####plt.scatter(xlist,ylist, c=t)
-    #plt.scatter(xlist, ylist, cmap=t)
-     ####for i,cc in zip(range(k), cluster_colors):
-     ####    plt.plot(centroids[i][0],centroids[i][1], '+', mew=10, ms=25,c='black')
-     ####    plt.plot(centroids[i][0],centroids[i][1], '+', mew=5, ms=20,c=cc)
-     ####plt.show()
-    return clusters
-    #print x, y
-#kmeans(l, 3)
-
-#from matplotlib import animation
-
-## create a simple animation
-#fig = plt.figure()
-#ax = plt.axes(xlim=(-4, 4), ylim=(-4, 4))
-#centroids = initialize_centroids(points, 3)
-
-#def init():
-#    return
-
-#def animate(i):
-#    global centroids
-#    closest = closest_centroid(points, centroids)
-#    centroids = move_centroids(points, closest, centroids)
-#    ax.cla()
-#    ax.scatter(points[:, 0], points[:, 1], c=closest)
-#    ax.scatter(centroids[:, 0], centroids[:, 1], c='r', s=100)
-#    return 
-
-#animation.FuncAnimation(fig, animate, init_func=init,
-#                        frames=10, interval=200, blit=True)
 
 if __name__ == '__main__':
-    #dataset = takeInput("input.txt", " ")
     dataset = takeInput()
-    #kmeans(dataset, 7)
-    bisectingKmeans(dataset, 7)
-    '''
-    nplist = np.asarray(dataset , dtype='float')
-    xlist = nplist[0: , 0]
-    ylist = nplist[0: , 1]
-    meanx = np.mean(xlist)
-    meany = np.mean(ylist)
-    t = np.arange(len(dataset))
-    plt.plot([meanx], [meany], '+', mew=10, ms=20)
-    plt.scatter(xlist, ylist, c=t)
-    plt.show()
-    '''
+    list_clusters = bisectingKmeans(dataset, 12, SIMULATION = True)  
+        
+    for i in xrange(len(list_clusters)):
+        nppoints = np.asarray(list_clusters[i].point_list, dtype = float)
+        if(len(nppoints)!=0):
+            plt.scatter(nppoints[0: , 0], nppoints[0: , 1], c=list_clusters[i].color)            
+        plt.plot(list_clusters[i].centroid[0],list_clusters[i].centroid[1], '+', mew=10, ms=25,c='black')
+        plt.plot(list_clusters[i].centroid[0],list_clusters[i].centroid[1], '+', mew=5, ms=20,c=list_clusters[i].color)
+    plt.title('After bisecting kmeans')
+    plt.show()  
     
-    #plt.plot(x, y)
-    #plt.show()
+    centroids = []
+    for cl in list_clusters:
+        centroids.append(cl.centroid)
 
-
+    kmeans(dataset, 12, centroids, True)
+    
